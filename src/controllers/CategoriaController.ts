4afdab5593a83685f3192dab9d498b518a544ca6
Pedro <pedro.ribeiro33@fatec.sp.gov.br>
2025-04-23 09:58:57 -0300

    feat: category update and delete

--- src/controllers/CategoriaController.ts
+++ src/controllers/CategoriaController.ts
@@ -26,4 +26,42 @@ export default class CategoriaController {
       res.status(500).json({ error: "Erro ao buscar categorias", details: error });
     }
   }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const deletedCategoria = await CategoriaModel.findByIdAndDelete(id);
+
+      if (!deletedCategoria) {
+        return res.status(404).json({ error: "Categoria não encontrada" });
+      }
+
+      res.status(200).json({ message: "Categoria deletada com sucesso", deletedCategoria });
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao deletar categoria", details: error });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { nome } = req.body;
+
+    if (!nome) {
+      return res.status(400).json({ error: "O campo nome é obrigatório" });
+    }
+
+    try {
+      const updatedCategoria = await CategoriaModel.findByIdAndUpdate(id, { nome }, { new: true });
+
+      if (!updatedCategoria) {
+        return res.status(404).json({ error: "Categoria não encontrada" });
+      }
+
+      res.status(200).json({ message: "Categoria atualizada com sucesso", updatedCategoria });
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao atualizar categoria", details: error });
+    }
+  }
+  
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,32 +6,38 @@ import DepartamentoController from "../controllers/DepartamentoController";
 import CategoriaController from "../controllers/CategoriaController";
 import PacoteController from "../controllers/PacoteController";
 
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const despesaController = new DespesaController()
+const despesaController = new DespesaController();
 const projetoController = new ProjetoController();
 const departamentoController = new DepartamentoController();
 const categoriaController = new CategoriaController();
 const pacoteController = new PacoteController();
 
+// Rotas de pacotes
 router.post('/pacote', pacoteController.create);
 router.get("/pacote", pacoteController.getAll);
 
+// Rotas de Despesas
 router.post("/despesa", despesaController.create);
 router.get("/despesa", despesaController.getAll);
 router.put("/despesa/:id", despesaController.aprovarDespesas);
 router.post('/despesas/by-ids', despesaController.getByIds);
 
-
+// Rotas de Projetos
 router.post("/projeto", projetoController.create);
 router.get("/projeto", projetoController.getAll);
 
+// Rotas de departamentos
 router.post("/departamentos", departamentoController.create);
 router.get("/departamentos", departamentoController.getAll);
 
+// Rotas de Categorias
 router.post("/categorias", categoriaController.create);
 router.get("/categorias", categoriaController.getAll);
+router.put('/categorias/:id', categoriaController.update);
+router.delete('/categorias/:id', categoriaController.delete);
 
 // Rotas do Usuário
 router.post("/register", UserController.register);
