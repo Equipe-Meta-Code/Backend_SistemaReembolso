845d7961cb633d40df70d8eb32c6b8208079ec6e
mluizaguede <mluizaguedessilva@gmail.com>
2025-05-25 11:54:28 -0300

    fix: correct misplaced comma in userModel

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
         twoFactorEnabled: {
             type: Boolean,
             default: false,
-        }
+        },
         role: {
             type: String,
             enum: ['usuario', 'gerente', 'admin'],
